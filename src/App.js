c9dc3101cdcc87fb9a6ab96c5f3607e4e4aa77f8
xdvz <imxras@gmail.com>
2021-06-10 15:46:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components"
+import React from "react";
+import "./App.scss";
+import Main from "./containers/Main";
 
-import original from "react95/dist/themes/original"
-import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2"
-import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2"
-import { styleReset } from "react95"
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
 
-import StartPanel from "./StartPanel"
-import Computer from "./Computer"
-import WindowsFrame from "./WindowsFrame"
-
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'ms_sans_serif', sans-serif;
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif', sans-serif;
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  
-  body {
-    font-family: 'ms_sans_serif', sans-serif;
-    background: teal;
-  }
-  ${styleReset}
-`
-
-const App = () => (
-  <>
-    <GlobalStyles />
-    <ThemeProvider theme={original}>
-      {/* <WindowsFrame /> */}
-      <Computer />
-      <StartPanel />
-    </ThemeProvider>
-  </>
-)
-
-export default App
+export default App;
--- src/ClassicButton.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Button } from "react95"
-
-const ClassicButton = () => <Button>Включить</Button>
-
-export default ClassicButton
